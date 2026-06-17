b987e1f73fc03928a25f2967dba3957d90251272
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-17 14:48:15 -0300

    Create RepositorioPets.h

---
+++ RepositorioPets.h
@@ -0,0 +1,20 @@
+#ifndef REPOSITORIOPETS_H
+#define REPOSITORIOPETS_H
+#include <vector>
+#include "Pet.h"
+
+    class RepositorioPets {
+        private:
+            std::vector<Pet> pets;
+            int ultimoId;
+
+        public:
+            RepositorioPets() : ultimoId(0) {}
+
+        int proximoId() { return ++ultimoId; }
+        void adicionar(const Pet& pet);
+        Pet buscarPorId(int id);
+        void listarPets();
+    };
+
+#endif
