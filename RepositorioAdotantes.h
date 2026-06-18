09522abcf61ad6e06324b1134b25ffae6bb3ab1d
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-18 18:09:43 -0300

    Create RepositorioAdotantes.h

---
+++ RepositorioAdotantes.h
@@ -0,0 +1,20 @@
+#ifndef REPOSITORIOADOTANTES_H
+#define REPOSITORIOADOTANTES_H
+#include <vector>
+#include "Adotante.h"
+
+    class RepositorioAdotantes {
+        private:
+            std::vector<Adotante> adotantes;
+            int ultimoId;
+
+    public:
+            RepositorioAdotantes() : ultimoId(0) {}
+
+            int proximoId() { return ++ultimoId; }
+    void adicionar(const Adotante& adotante);
+        Adotante buscarPorId(int id);
+    void listarAdotantes();
+};
+
+#endif
