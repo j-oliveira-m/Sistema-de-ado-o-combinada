3b6c92dedf82ccafd65a031261b7278ea7356f91
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-18 18:09:28 -0300

    Create RepositorioPets.cpp

---
+++ RepositorioPets.cpp
@@ -0,0 +1,22 @@
+#include "RepositorioPets.h"
+#include <iostream>
+
+    using namespace std;
+
+    void RepositorioPets::adicionar(const Pet& pet){
+        pets.push_back(pet);
+    }
+
+    Pet RepositorioPets::buscarPorId(int id) {
+        for (auto& p : pets){
+            if(p.getId() == id) return p;
+        }
+        cout << "Pet nao encontrado!\n";
+        return Pet();
+    }
+
+    void RepositorioPets::listarPets() {
+        for (auto& p : pets){
+            p.exibirPet();
+        }
+    }
