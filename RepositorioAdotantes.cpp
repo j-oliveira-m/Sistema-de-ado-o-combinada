6a3edb4166b1cf1de9b81cb051d676f1da6a4fe1
oliveira._.j <juliamatosdeoliveira51@gmail.com>
2026-06-19 14:34:55 -0300

    Update RepositorioAdotantes.cpp

--- RepositorioAdotantes.cpp
+++ RepositorioAdotantes.cpp
@@ -2,19 +2,23 @@
 #include <iostream>
 using namespace std;
 
-    void RepositorioAdotantes::adicionar(const Adotante& adotante) {
-        adotantes.push_back(adotante);
-}
-        Adotante RepositorioAdotantes::buscarPorId(int id) {
-            for (auto& a : adotantes) {
-                if (a.getId() == id) return a;
-    }
-            cout << "Adotante nao encontrado!\n";
-            return Adotante(); // usa o construtor vazio
+void RepositorioAdotantes::adicionar(const Adotante& adotante) {
+    adotantes.push_back(adotante);
 }
 
-    void RepositorioAdotantes::listarAdotantes() {
-        for (auto& a : adotantes) {
-            a.exibirAdotante();
+Adotante RepositorioAdotantes::buscarPorId(int id) {
+    for (auto& a : adotantes) {
+        if (a.getId() == id) return a;
+    }
+    cout << "Adotante nao encontrado!\n";
+    return Adotante(); // usa o construtor vazio
+}
+
+void RepositorioAdotantes::listarAdotantes() {
+    for (auto& a : adotantes) {
+        a.exibirAdotante();
     }
 }
+
+
+
